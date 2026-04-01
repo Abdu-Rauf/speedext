c2c899119e1a318d84fe6d0879f4df3f5383d65e
Abdu-Rauf <abdurraufaamir@gmail.com>
2026-04-01 13:20:12 +0530

    Run Root with all childrens

--- main.go
+++ main.go
@@ -1,21 +1,9 @@
 package main
 
 import (
-	"log"
-
-	"github.com/Abdu-Rauf/speedext/speedtest"
+	"github.com/Abdu-Rauf/speedext/cmd"
 )
 
 func main() {
-	log.Println("Starting speedext test...")
-
-	log.Println("--- Running Download Test ---")
-	downloadResult := speedtest.RunDownload()
-	log.Println(downloadResult)
-
-	log.Println("--- Running Upload Test ---")
-	uploadResult := speedtest.RunUpload()
-	log.Println(uploadResult)
-
-	log.Println("Test complete.")
+	cmd.Execute()
 }
